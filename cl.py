77bab22c1dfba32d444bb146b492e01a10982e3d
Volodymyr <futuimavolodymyr777@gmail.com>
2026-02-15 13:21:35 +0100

    dfg456

--- cl.py
+++ cl.py
@@ -1,34 +1,35 @@
-#юзер вводит список чисел
+#юзер вводит числа через пробел
+#map(int, ...) прогоняет каждое значение и делает из строки число
 numbers = list(map(int, input("введи числа через пробел: ").split()))
 
-#считаем сумму
+#sum() сама пробегается по списку и складывает всё в кучу
 total = sum(numbers)
 
-#считаем среднее арифметическое
 average = total / len(numbers) if numbers else 0
 
 print("сумма всех элементов:", total)
 print("среднее арифметическое:", average)
 
-#юзер вводит число которое нужно найти
+
 target = int(input("введи число которое ищем: "))
 
-#считаем сколько раз оно встречается
 count = numbers.count(target)
 
 print("сколько раз встречается число:", count)
 
-#считаем сумму положительных чисел
+
+#тут фишка: sum() + генератор
+#он на лету перебирает список и берет только то что больше нуля
 positive_sum = sum(num for num in numbers if num > 0)
 
 print("сумма положительных чисел:", positive_sum)
 
-#ищем индексы всех четных чисел
+
 even_indices = [i for i, num in enumerate(numbers) if num % 2 == 0]
 
 print("индексы четных чисел:", even_indices)
 
-#оставляем только уникальные элементы без повторов
+
 unique_numbers = []
 for num in numbers:
     if numbers.count(num) == 1:
